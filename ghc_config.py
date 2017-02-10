e5c25edae76c4507ea73a11df3db8c88ca2ed390
Nobuo Yamashita <nobsun@sampou.org>
2017-02-10 11:06:06 +0900

    start gh-pages

--- ghc_config.py
+++
@@ -1,9 +0,0 @@
-extlinks = {
-    'base-ref': ('../libraries/base-4.9.1.0/%s', ''),
-    'cabal-ref': ('../libraries/Cabal-1.24.2.0/%s', ''),
-    'ghc-prim-ref': ('../libraries/ghc-prim-0.5.0.0/%s', ''),
-    'ghc-ticket': ('http://ghc.haskell.org/trac/ghc/ticket/%s', 'Trac #'),
-    'ghc-wiki': ('http://ghc.haskell.org/trac/ghc/wiki/%s', 'Trac #'),
-}
-
-version = '8.0.2'
