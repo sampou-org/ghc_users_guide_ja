4dc3fd069c7b466459fdea493169aaf3e779da00
Nobuo Yamashita <nobsun@sampou.org>
2017-12-22 18:01:43 +0900

    add 8.2.2

---
+++ 8.2.2/users_guide/ghc_config.py
@@ -0,0 +1,12 @@
+extlinks = {
+    'base-ref': ('../libraries/base-4.10.1.0/%s', ''),
+    'cabal-ref': ('../libraries/Cabal-2.0.1.0/%s', ''),
+    'compact-ref': ('../libraries/ghc-compact-0.1.0.0/%s', ''),
+    'ghc-prim-ref': ('../libraries/ghc-prim-0.5.1.1/%s', ''),
+    'th-ref': ('../libraries/template-haskell-@LIBRARY_template_haskell_VERSION@/%s', ''),
+
+    'ghc-ticket': ('https://ghc.haskell.org/trac/ghc/ticket/%s', 'Trac #'),
+    'ghc-wiki': ('https://ghc.haskell.org/trac/ghc/wiki/%s', 'Trac #'),
+}
+
+version = '8.2.2'
