6ade9eec8bc93f75148ad1419c827835febdd937
Nobuo Yamashita <nobsun@sampou.org>
2018-01-31 18:00:08 +0900

    translate 7.6.14

--- 8.2.2/mkUserGuidePart/Options/OptimizationLevels.hs
+++ 8.2.2/mkUserGuidePart/Options/OptimizationLevels.hs
@@ -5,25 +5,25 @@ import Types
 optimizationLevelsOptions :: [Flag]
 optimizationLevelsOptions =
   [ flag { flagName = "-O0"
-         , flagDescription = "Disable optimisations (default)"
+         , flagDescription = "最適化を無効にする(デフォルト)"
          , flagType = DynamicFlag
          , flagReverse = "-O"
          }
   , flag { flagName = "-O, -O1"
-         , flagDescription = "Enable level 1 optimisations"
+         , flagDescription = "レベル 1 の最適化を有効にする"
          , flagType = DynamicFlag
          , flagReverse = "-O0"
          }
   , flag { flagName = "-O2"
-         , flagDescription = "Enable level 2 optimisations"
+         , flagDescription = "レベル 2 の最適化を有効にする"
          , flagType = DynamicFlag
          , flagReverse = "-O0"
          }
   , flag { flagName = "-Odph"
          , flagDescription =
-           "Enable level 2 optimisations, set "++
-           "``-fmax-simplifier-iterations=20`` "++
-           "and ``-fsimplifier-phases=3``."
+           "レベル 2 の最適化を有効にした上で， "++
+           "``-fmax-simplifier-iterations=20`` および "++
+           "``-fsimplifier-phases=3`` を設定する"
          , flagType = DynamicFlag
          }
   ]
